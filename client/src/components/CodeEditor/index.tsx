8a04c2e82669efb44748723ef30330c727102f85
Jim Sangwine <jim@sangwine.net>
2022-11-24 15:17:04 +0000

    Docker setup & initial mock routes WIP

---
+++ client/src/components/CodeEditor/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Editor from 'react-simple-code-editor';
+// @ts-ignore
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-json';
+import 'prismjs/themes/prism-dark.css';
+
+function App() {
+  const [code, setCode] = React.useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
+  return (
+    <Editor
+      value={code}
+      onValueChange={code => setCode(code)}
+      highlight={code => highlight(code, languages.js)}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 12,
+      }}
+    />
+  );
+}
